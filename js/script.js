9c9968905ba8d0ef5a4391ddb4cfe344236a4618
Adappt <Adappt>
2017-09-04 19:41:19 +0530

    Completed Homepage and Galleria

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// Global Declaration
+var ALBUM_URL="https://jsonplaceholder.typicode.com/albums/";
+var ALBUM_ID="/photos";
+
+
+$(document).ready(function(){
+  callHomepage();
+});
+
+function callHomepage(){
+  $(".backButton-info").hide();
+  $.get('templatehome.html',function(template){
+    $.getJSON(ALBUM_URL,function(data){
+      var result=data;
+      template=_.template(template, {variable: 'data'})({res: result}, getFirstImageByAlbum);
+      $(".albums-rows").html(template);
+    });
+  });
+}
+
+// getAlbumID
+function getFirstImageByAlbum(albumId) {
+  var link = ALBUM_URL + albumId + ALBUM_ID ; 
+  var testImage = $.getJSON(link);
+  testImage.done(function(data) {
+      $('.albums img[data-id='+albumId+']').attr('src', data[0].thumbnailUrl);
+  });
+}
+
+// getPhotos
+function getPhotos(id) {
+
+    $(".backButton-info").show();
+    $.get('templatephotos.html',function(template){
+        var photos = ALBUM_URL + id + ALBUM_ID ;
+      $.getJSON(photos,function(data){
+        var result=data;
+        template=_.template(template, {variable: 'data'})({res: result});
+        $(".albums-rows").html(template);
+        Galleria.loadTheme('themes/classic/galleria.classic.min.js');
+        Galleria.run('#galleria');
+      });
+    });
+}
+
+
+
+
+
+//   $.get('photos.html',function(template){
+//       var photos = ALBUM_URL + id + ALBUM_ID ; 
+//       $.getJSON(photos,function(data){
+//         var allPhotos=data;
+//         template=_.template(template, {variable: 'data'})({res: result},);
+//       //photos.done(function(data) {
+//         //console.log(photos);
+//         $('.photos img[data-id='+albumId+']').attr('src', data.url);
+//       });
+//   });
+//   //});
+// }
